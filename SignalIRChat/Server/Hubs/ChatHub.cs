b377f62c88f3688006eb4ecf7fea58d5ebbafac1
Ramses Rmz <a00344520@tec.mx>
2022-10-30 23:03:05 -0600

    Add project files.

---
+++ SignalIRChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalIRChat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            await AddMessageToChat("", "The user is connected");
+            await base.OnConnectedAsync();
+        }
+
+        public async Task AddMessageToChat(string user, string message)
+        {
+            // El 1er parametro es para identificar el tipo de mensaje en el servidor
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
